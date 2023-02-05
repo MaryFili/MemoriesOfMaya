4d6cd59e20444cd2916575c49e4f853ee983d744
MaryF <filardimary@gmail.com>
2023-02-05 12:42:43 +0100

    first draft

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'A blog tribute to an amazing friend';
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
